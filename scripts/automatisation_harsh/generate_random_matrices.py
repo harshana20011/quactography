cd3921175cee540478cee61a26da0379a68ca228
harshana20011 <runh1501.usherbrooke.ca>
2024-02-17 15:36:17 -0500

    matrice aléatoire étrange

--- scripts/automatisation_harsh/generate_random_matrices.py
+++ scripts/automatisation_harsh/generate_random_matrices.py
@@ -2,28 +2,34 @@ import numpy as np
 import pandas as pd
 
 
-def generate_random_adjacency_matrix(num_nodes, max_weight=10):
+def generate_random_adjacency_matrix(num_nodes, max_weight=0.5):
     """
     Génère une matrice d'adjacence symétrique avec des poids aléatoires.
 
     Args:
         num_nodes (int): Nombre de nœuds dans le graphe.
-        max_weight (int): Poids maximum autorisé pour les arêtes.
+        max_weight (float): Poids maximum autorisé pour les arêtes.
 
     Returns:
         np.ndarray: Matrice d'adjacence générée.
     """
     # Matrice vide de taille num_nodes x num_nodes:
-    adjacency_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
+    adjacency_matrix = np.zeros((num_nodes, num_nodes), dtype=float)
 
     # Remplir la matrice avec des poids aléatoires pour les arêtes
     for i in range(num_nodes):
         for j in range(i + 1, num_nodes):
-            weight = np.random.randint(
-                1, max_weight + 1
-            )  # Poids aléatoire entre 1 et max_weight
+            weight = np.random.uniform(
+                0, max_weight
+            )  # Poids aléatoire entre 0 et max_weight
             adjacency_matrix[i, j] = weight
             adjacency_matrix[j, i] = weight  # La matrice est symétrique
+            # Mettez à zéro au moins la moitié des connexions supplémentaires
+    num_zeros_to_add = 30
+    for _ in range(num_zeros_to_add):
+        i, j = np.random.randint(0, num_nodes, size=2)
+        adjacency_matrix[i, j] = 0
+        adjacency_matrix[j, i] = 0
 
     return adjacency_matrix
 
