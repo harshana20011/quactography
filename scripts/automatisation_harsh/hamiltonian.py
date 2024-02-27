ad35b5baaa187fff3255bc3b610d6bb786dc4514
harshana20011 <runh1501.usherbrooke.ca>
2024-02-27 10:44:08 -0500

    reste a voir pour tracer les alphas

--- scripts/automatisation_harsh/hamiltonian.py
+++ scripts/automatisation_harsh/hamiltonian.py
@@ -100,3 +100,70 @@ def hfin(noeud_de_fin, depart, q_indices, destination, number_of_edges):
 
     print(f"\n Contrainte de fin = {h_fin}")
     return h_fin
+
+
+def hint(
+    noeud_de_depart, noeud_de_fin, depart, q_indices, destination, number_of_edges
+):
+    """Générer tous les termes intermédiaires et les sommer
+
+    Args:
+        noeud_de_depart (int): entrée décicé par l'utilisateur : noeud de départ
+        noeud_de_fin (int):fin décicé par l'utilisateur : noeud de fin
+        depart (list int): Liste de noeuds en départ (en fonction de la matrice d'adjacence pour éviter les doublements)
+        q_indices (list int): Indice associé à chaque qubit en fonction de la matrice d'adjacence
+        destination (list int):  Liste de noeuds en fin (en fonction de la matrice d'adjacence pour éviter les doublements)
+        number_of_edges (int): nombre de edges qui est le même que le nombre de qubits dans le graphe
+
+    Returns:
+        Somme sur les contraintes intermédiaires de chaque noeud au carré (somme finale)
+    """
+    # Contrainte intermédiaire:
+    noeuds_int = []
+    for node, value in enumerate(depart):
+        if (value != noeud_de_depart) and (value != noeud_de_fin):
+            if value not in noeuds_int:
+                noeuds_int.append(depart[node])
+    for node, value in enumerate(destination):
+        if (value != noeud_de_depart) and (value != noeud_de_fin):
+            if value not in noeuds_int:
+                noeuds_int.append(destination[node])
+
+    print(f"Liste de noeuds intermédiaires: {noeuds_int} \n")
+
+    liste_qubits_int = [[] for _ in range(len(noeuds_int))]
+    for i, node in enumerate(noeuds_int):
+        for node, value in enumerate(destination):
+            if value == noeuds_int[i]:
+                liste_qubits_int[i].append(q_indices[node])
+        for node, value in enumerate(depart):
+            if value == noeuds_int[i]:
+                liste_qubits_int[i].append(q_indices[node])
+
+    for i in range(len(liste_qubits_int)):
+        a = liste_qubits_int[i]
+        print(f"Qubit à multiplier sur les x_i intermédiaires: q({a}) ")
+
+    hint = []
+    prod_terms = []
+    for list in liste_qubits_int:
+        prod_term = "I" * number_of_edges
+        for qubit in list:
+            prod_term = prod_term[:qubit] + "Z" + prod_term[qubit + 1 :]
+        prod_terms.append(prod_term[::-1])
+
+    for i in range(len(liste_qubits_int)):
+        hint.append([])
+
+        hint[i] = SparsePauliOp.from_list(
+            [("I" * number_of_edges, -1.0), (prod_terms[i], 1.0)]
+        )
+
+    print(f"\n Contrainte intermédiaire = {hint}")
+
+    hints = []
+    for i in range(len(hint)):
+        hints.append([])
+        hints[i] = hint[i] ** 2
+    print(f"Somme sur les termes intermédiaires au carré: {sum(hints[i])}")
+    return sum(hints[i])
--- scripts/automatisation_harsh/main.py
+++ scripts/automatisation_harsh/main.py
@@ -1,12 +1,7 @@
-# from qiskit.visualization import plot_distribution
-# from qiskit.primitives import Estimator, Sampler
-from qiskit.quantum_info import SparsePauliOp
-
-# from qiskit.circuit.library import QAOAAnsatz
-# from scipy.optimize import minimize
-
-# import matplotlib.pyplot as plt
-# import networkx as nx
+from qiskit.visualization import plot_distribution
+from qiskit.primitives import Estimator, Sampler
+from qiskit.circuit.library import QAOAAnsatz
+from scipy.optimize import minimize
 import pandas as pd
 import numpy as np
 
@@ -14,7 +9,7 @@ from visualisation_entry_graph import visualize_num_nodes
 from connexions_qubits import connexions_edges
 from generate_random_matrices import generate_random_adjacency_matrix
 from generate_random_matrices import save_adjacency_matrix_to_csv
-from hamiltonian import hc, hdep, hfin
+from hamiltonian import hc, hdep, hfin, hint
 
 # Code pour matrice d'adjacence déjà existante :
 # df = pd.read_csv(r"scripts\automatisation_harsh\matrices\mat_adj.csv")
@@ -38,15 +33,63 @@ number_of_edges, weights, depart, destination, q_indices, all_weights_sum = (
 )
 
 # Le coût obligatoire (premier terme de Hamiltonien) :
-hc(number_of_edges, weights, all_weights_sum)
+hc = hc(number_of_edges, weights, all_weights_sum)
 
 # Le terme associé au respect de la contrainte de départ :
 # Déterminer le noeud de départ :
 noeud_de_depart = 0
-hdep(noeud_de_depart, depart, q_indices, destination, number_of_edges)
+hdep = hdep(noeud_de_depart, depart, q_indices, destination, number_of_edges)
 
 
 # Le terme associé au respect de la contrainte de fin :
 # Déterminer le noeud de fin :
 noeud_de_fin = 5
-hfin(noeud_de_fin, depart, q_indices, destination, number_of_edges)
+hfin = hfin(noeud_de_fin, depart, q_indices, destination, number_of_edges)
+
+hint = hint(
+    noeud_de_depart, noeud_de_fin, depart, q_indices, destination, number_of_edges
+)
+
+# Alpha : coût associé aux contraintes:
+alpha = 0.8
+# Fonction coût en représentation QUBO:
+h = -hc + alpha * ((hdep**2) + (hfin**2) + hint)
+
+
+# Create QAOA circuit.
+ansatz = QAOAAnsatz(h, reps=1)
+ansatz.decompose(reps=3).draw()
+
+# Run on local estimator and sampler. Fix seeds for results reproducibility.
+estimator = Estimator(options={"shots": 1000000, "seed": 42})
+sampler = Sampler(options={"shots": 1000000, "seed": 42})
+
+
+# Cost function for the minimizer.
+# Returns the expectation value of circuit with Hamiltonian as an observable.
+def cost_func(params, estimator, ansatz, hamiltonian):
+    cost = (
+        estimator.run(ansatz, hamiltonian, parameter_values=params).result().values[0]
+    )
+    return cost
+
+
+# Generate starting point. Fixed to zeros for results reproducibility.
+# x0 = 2 * np.pi * np.random.rand(ansatz.num_parameters)
+x0 = np.zeros(ansatz.num_parameters)
+
+res = minimize(cost_func, x0, args=(estimator, ansatz, h), method="COBYLA")
+print(res)
+
+# cost_func([ 8.250e-01  ,2.622e-01  ,5.591e-02 ,-2.605e-01],estimator, ansatz, h)
+
+# Get probability distribution associated with optimized parameters.
+circ = ansatz.copy()
+circ.measure_all()
+dist = sampler.run(circ, res.x).result().quasi_dists[0]
+
+
+plot_distribution(dist.binary_probabilities(), figsize=(8, 8))
+
+print(max(dist.binary_probabilities(), key=dist.binary_probabilities().get))  # type: ignore
+print(sorted(dist.binary_probabilities(), key=dist.binary_probabilities().get))  # type: ignore
