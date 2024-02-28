ddac13078d4b066405a0fe835135b11d68361c3c
harshana20011 <runh1501.usherbrooke.ca>
2024-02-28 09:17:40 -0500

    Resultats

--- scripts/automatisation_harsh/main.py
+++ scripts/automatisation_harsh/main.py
@@ -14,18 +14,18 @@ from generate_random_matrices import generate_random_adjacency_matrix
 from generate_random_matrices import save_adjacency_matrix_to_csv
 from hamiltonian import hc, hdep, hfin, hint
 
-# Code pour matrice d'adjacence déjà existante :
-df = pd.read_csv(r"scripts\automatisation_harsh\matrices\mat_adj.csv")
-mat_adj = np.array(df)
+# # Code pour matrice d'adjacence déjà existante :
+# df = pd.read_csv(r"scripts\automatisation_harsh\matrices\mat_adj.csv")
+# mat_adj = np.array(df)
 
-# # Code pour une matrice générée aléatoirement :
-# num_nodes = 8
-# random_adj_matrix = generate_random_adjacency_matrix(num_nodes, num_zeros_to_add=20)
-# mat_adj = np.array(random_adj_matrix)
-# save_adjacency_matrix_to_csv(random_adj_matrix, filename="random_adjacency_matrix.csv")
+# Code pour une matrice générée aléatoirement :
+num_nodes = 8
+random_adj_matrix = generate_random_adjacency_matrix(num_nodes, num_zeros_to_add=20)
+mat_adj = np.array(random_adj_matrix)
+save_adjacency_matrix_to_csv(random_adj_matrix, filename="random_adjacency_matrix.csv")
 
-# # Lire la matrice :
-# df = pd.read_csv(r"random_adjacency_matrix.csv")
+# Lire la matrice :
+df = pd.read_csv(r"random_adjacency_matrix.csv")
 
 # Visualisation et détermination du nombre de noeuds dans le graphe :
 num_nodes = visualize_num_nodes(df, mat_adj)
